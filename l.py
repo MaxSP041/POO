98d2853eb15e05b42f0ea49ba79dd3792bf6b4ed
MaxSP041 <l24590415@sjuanrio.tecnm.mx>
2025-02-27 09:31:03 -0600

    Programa hecho en clase por Stringhini el 27/02/25

--- l.py
+++ l.py
@@ -1,20 +1,64 @@
-"""
-mi_lista = ['Juan', 'Pedro', 'Laura', 'Carmen', 'Susana']
-x="hola mundo" 
-y=list(['Juan', 'Pedro', 'Laura', 'Carmen', 'Susana'])print(mi_lista[0]) # Muestra Juan (la primera posición es la 0) 
+nombre_de_la_lista = [] 
+otra_lista = list() 
+lista_con_elementos = [1, 2, True, 'Hola', 5.8] 
+otra_lista_con_elementos = list([4, 9, False, 'texto'])
+mi_lista = ['Juan', 'Pedro', 'Laura', 'Carmen', 'Susana'] 
+print(mi_lista[0]) # Muestra Juan (la primera posición es la 0) 
 print(mi_lista[-1]) # Muestra Susana 
 print(mi_lista[1]) # Muestra Pedro 
 print(mi_lista[2]) # Muestra Laura 
-print(mi_lista[-2]) # Muestra Carmen 
-print(mi_lista[-3]) # Muestra Carmen 
-print(mi_lista[-2]) # Muestra Carmen print(len(mi_lista))
-print(len(x))
-print(len(y))
-"""
+print(mi_lista[-2]) # Muestra Carmen
 edades = [20, 41, 6, 18, 23] 
 # Recorriendo los elementos 
-#for edad in edades: 
-    #print(edad)
-#print(edades)
-for i in range(1,len(edades),2):
-    print(edades[i])
+for edad in edades: 
+    print(edad) 
+# Recorriendo los índices 
+for i in range(len(edades)): 
+    print(edades[i]) 
+# Con while y los índices 
+indice = 0 
+while indice < len(edades): 
+    print(edades[indice]) 
+    indice += 1
+    
+números = [] 
+números.append(10) 
+números.append(5) 
+números.append(3) 
+print(números) 
+# Mostrará [10, 5, 3]
+
+números = [] 
+# Unimos la lista anterior con una nueva 
+números = números + [10, 5, 3] 
+print(números) 
+# Mostrará [10, 5, 3]
+
+palabras = ['hola', 'hello', 'ola'] 
+palabras.pop(-5) 
+print(palabras) 
+# Mostrará ['hola', 'ola']
+
+palabras = ['hola', 'hello', 'hello', 'ola'] 
+palabras.remove('hello') 
+print(palabras) 
+# Mostrará ['hola', 'hello', 'ola']
+
+# Creamos las listas (vacías al comienzo) 
+nombres = [] 
+identificaciones = [] 
+# Definimos un tamaño para las listas 
+# Lo puedes cambiar si quieres 
+tamaño = 3 
+# Leemos los datos y los agregamos a la lista 
+for i in range(tamaño): 
+    print("Ingrese los datos de la persona", i + 1) 
+    nombre = input("Nombre: ") 
+    identificación = input("Identificación: ") 
+    nombres.append(nombre) 
+    identificaciones.append(identificación) 
+# Ahora mostremos las listas 
+for i in range(tamaño): 
+    print("Mostrando los datos de la persona", i + 1) 
+    print("Nombre:", nombres[i]) 
+    print("Identificación:", identificaciones[i])
