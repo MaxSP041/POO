333349361157cdbd3ce12345fd5974789407fad2
MaxSP041 <l24590415@sjuanrio.tecnm.mx>
2025-02-27 09:35:49 -0600

    programa hecho el 21 de febrero en clase

---
+++ m.py
@@ -0,0 +1,7 @@
+import random
+x="hola mundo tengo hasta diciembre apa"#vector
+print(len(x))#len -longitud
+#y=x.replace("a","a") no jala
+#print(random[0,35]) no jala
+u=random.randint(0,35)
+print(u)
